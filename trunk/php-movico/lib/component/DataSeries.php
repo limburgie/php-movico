e60334f245b479fb57596cc4313733b655e4c70f
limburgie <limburgie@00edc556-cc59-0846-f5fe-e7f7f7779471>
2011-05-06 21:44:44 +0000

    DataTable pagination

--- trunk/php-movico/lib/component/DataSeries.php
+++ trunk/php-movico/lib/component/DataSeries.php
@@ -3,21 +3,30 @@ abstract class DataSeries extends Component {
 	
 	protected $value;
 	protected $var;
+	protected $rows;
+	
+	public function getValue() {
+		return $this->value;
+	}
 	
 	public function setValue($value) {
 		$this->value = $value;
-	}
-	
-	public function setVar($var) {
-		$this->var = $var;
 	}
 	
 	public function getVar() {
 		return $this->var;
 	}
 	
-	public function getValue() {
-		return $this->value;
+	public function setVar($var) {
+		$this->var = $var;
+	}
+	
+	public function getRows() {
+		return $this->rows;
+	}
+	
+	public function setRows($rows) {
+		$this->rows = $rows;
 	}
 	
 }
