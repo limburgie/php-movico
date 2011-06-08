61bd70d00fa314a593c85f5b18089080b4465f9c
limburgie@gmail.com <limburgie@gmail.com@00edc556-cc59-0846-f5fe-e7f7f7779471>
2011-06-08 15:33:17 +0000

    46: upgraded to jQuery 1.6.1

--- trunk/php-movico/lib/component/View.php
+++ trunk/php-movico/lib/component/View.php
@@ -13,7 +13,7 @@ class View extends Component {
 	public function doRender($index=null) {
 		$ajax = Singleton::create("Settings")->isAjaxEnabled();
 		$result = "<html>\n\t<head>\n\t\t<title>".$this->title."</title>\n".
-			"<script type=\"text/javascript\" src=\"lib/javascript/jquery-1.5.2.min.js\"></script>".
+			"<script type=\"text/javascript\" src=\"lib/javascript/jquery-1.6.1.min.js\"></script>".
 			"<script type=\"text/javascript\" src=\"lib/javascript/forms.js\"></script>";
 		if($ajax) {
 			$ajaxTimeout = Singleton::create("Settings")->getAjaxTimeout();
