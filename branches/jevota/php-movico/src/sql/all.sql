d361cdf8da59ae09ccfd24885c0bfb97c893d7a5
limburgie@gmail.com <limburgie@gmail.com@00edc556-cc59-0846-f5fe-e7f7f7779471>
2011-07-22 08:32:27 +0000

    Fixed faulty column name phone in data.sql

--- branches/jevota/php-movico/src/sql/all.sql
+++ branches/jevota/php-movico/src/sql/all.sql
@@ -46,4 +46,4 @@ CREATE TABLE `PingpongGame` (
 
 INSERT INTO `Account` (emailAddress, password) VALUES ('admin@jevota.be', 'admin123');
 
-INSERT INTO `PingpongClub` (number, name, shortName, address, telephone) VALUES ('31', 'T.T.C. Jevota', 'Lanaken', 'Biesweg 16, Gellik', '089/714535');
+INSERT INTO `PingpongClub` (number, name, shortName, address, phone) VALUES ('31', 'T.T.C. Jevota', 'Lanaken', 'Biesweg 16, Gellik', '089/71.45.35');
