fa7e844cc6bb2a6810943cc4fecc4f161b108f70
limburgie@gmail.com <limburgie@gmail.com@00edc556-cc59-0846-f5fe-e7f7f7779471>
2011-08-05 20:38:42 +0000

    Updated data sql

--- branches/jevota/php-movico/src/sql/all.sql
+++ branches/jevota/php-movico/src/sql/all.sql
@@ -48,4 +48,4 @@ CREATE TABLE `PingpongGame` (
 
 INSERT INTO `Account` (emailAddress, password) VALUES ('admin@jevota.be', 'admin123');
 
-INSERT INTO `PingpongClub` (number, name, shortName, street, place, phone) VALUES ('31', 'T.T.C. Jevota', 'Lanaken', 'Biesweg 16, '3620 Gellik', '089/71.45.35');
+INSERT INTO `PingpongClub` (number, name, shortName, street, place, phone) VALUES ('31', 'T.T.C. Jevota', 'Lanaken', 'Biesweg 16', '3620 Gellik', '089/71.45.35');
--- branches/jevota/php-movico/src/sql/data.sql
+++ branches/jevota/php-movico/src/sql/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO `Account` (emailAddress, password) VALUES ('admin@jevota.be', 'admin123');
 
-INSERT INTO `PingpongClub` (number, name, shortName, street, place, phone) VALUES ('31', 'T.T.C. Jevota', 'Lanaken', 'Biesweg 16, '3620 Gellik', '089/71.45.35');
+INSERT INTO `PingpongClub` (number, name, shortName, street, place, phone) VALUES ('31', 'T.T.C. Jevota', 'Lanaken', 'Biesweg 16', '3620 Gellik', '089/71.45.35');
