6c901d9c4ac09a14b769308e7f8c9b6f05f90169
limburgie@gmail.com <limburgie@gmail.com@00edc556-cc59-0846-f5fe-e7f7f7779471>
2011-08-20 12:46:08 +0000

    Merging with Jevota stuff

--- php-movico/lib/component/Form.php
+++ php-movico/lib/component/Form.php
@@ -7,7 +7,7 @@ class Form extends Component {
 		$target = $isFileUpload ? "fileUpload".$this->id : "_self";
 		$context = parent::$settings->getContextPath()."/index.php";
 		$action = $isFileUpload ? "$context?upload=1" : $context;
-		$result = "<form enctype=\"$enctype\" name=\"form".$this->id."\" id=\"".$this->id."\" method=\"post\" action=\"$action\" target=\"$target\">";
+		$result = "<form".$this->getClassStr()." enctype=\"$enctype\" name=\"form".$this->id."\" id=\"".$this->id."\" method=\"post\" action=\"$action\" target=\"$target\">";
 		$result .= $this->renderChildren();
 		$result .= "<input type=\"hidden\" name=\"".MovicoRequest::ACTION."\"/>";
 		$result .= "<input type=\"hidden\" name=\"".MovicoRequest::PREV_URL."\" value=\"".$this->getViewId()."\"/>";
---
+++ php-movico/lib/database/cache/DatabaseCache.php
@@ -0,0 +1,97 @@
+<?php
+class DatabaseCache extends ApplicationBean {
+	
+	private $cache = array();
+	private $enabled;
+	
+	const ALL = "All";
+	const FINDER = "Finder";
+	const SINGLE = "Single";
+	
+	public function __construct() {
+		$this->enabled = Singleton::create("Settings")->isDbCacheEnabled();
+	}
+
+	public function getAll($entity) {
+		return $this->cache[$entity][self::ALL];
+	}
+	
+	public function hasAll($entity) {
+		if(!$this->enabled) {
+			return false;
+		}
+		return isset($this->cache[$entity][self::ALL]);
+	}
+	
+	public function setAll($entity, $objects) {
+		if(!$this->enabled) {
+			return;
+		}
+		$this->cache[$entity][self::ALL] = $objects;
+		BeanLocator::storeBean($this);
+	}
+	
+	public function resetEntity($entity) {
+		if(!$this->enabled) {
+			return;
+		}
+		unset($this->cache[$entity][self::ALL]);
+		unset($this->cache[$entity][self::FINDER]);
+		BeanLocator::storeBean($this);
+	}
+	
+	public function getFinder($entity, $finderQuery) {
+		return $this->cache[$entity][self::FINDER][$finderQuery];
+	}
+	
+	public function hasFinder($entity, $finderQuery) {
+		if(!$this->enabled) {
+			return false;
+		}
+		return isset($this->cache[$entity][self::FINDER][$finderQuery]);
+	}
+	
+	public function setFinder($entity, $finderQuery, $objects) {
+		if(!$this->enabled) {
+			return;
+		}
+		$this->cache[$entity][self::FINDER][$finderQuery] = $objects;
+		BeanLocator::storeBean($this);
+	}
+	
+	public function getSingle($entity, $id) {
+		return $this->cache[$entity][self::SINGLE][$id];
+	}
+	
+	public function hasSingle($entity, $id) {
+		if(!$this->enabled) {
+			return false;
+		}
+		return isset($this->cache[$entity][self::SINGLE][$id]);
+	}
+	
+	public function setSingle($entity, $id, $object) {
+		if(!$this->enabled) {
+			return;
+		}
+		$this->cache[$entity][self::SINGLE][$id] = $object;
+		BeanLocator::storeBean($this);
+	}
+	
+	public function resetSingle($entity, $id) {
+		if(!$this->enabled) {
+			return;
+		}
+		unset($this->cache[$entity][self::SINGLE][$id]);
+		BeanLocator::storeBean($this);
+	}
+	
+	private function getFinderKey($args) {
+		if(count($args) == 1) {
+			return self::ALL;
+		}
+		return $args[1];
+	}
+	
+}
+?>
