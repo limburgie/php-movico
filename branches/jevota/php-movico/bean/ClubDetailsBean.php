9953cd69df9d81838eeae06704e916fb03a8240f
limburgie@gmail.com <limburgie@gmail.com@00edc556-cc59-0846-f5fe-e7f7f7779471>
2011-09-13 21:25:03 +0000

    Fixed issue with jevotaClubId

--- branches/jevota/php-movico/bean/ClubDetailsBean.php
+++ branches/jevota/php-movico/bean/ClubDetailsBean.php
@@ -4,18 +4,12 @@ class ClubDetailsBean extends RequestBean {
 	private $club = null;
 	
 	public function __construct() {
-		if(Context::hasParam(0)) {
-			$clubId = Context::getParam(0);
-			$this->club = PingpongClubServiceUtil::getPingpongClub($clubId);
-		}
+		$clubId = Context::getParam(0);
+		$this->club = PingpongClubServiceUtil::getPingpongClub($clubId);
 	}
 	
 	public function getClub() {
 		return $this->club;
-	}
-	
-	public function getJevotaClubId() {
-		return PingpongClubServiceUtil::getJevota()->getClubId();
 	}
 	
 }
